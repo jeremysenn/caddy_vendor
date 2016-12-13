0e21571cdb7f51f2d9f3e365703e617b13399834
Jeremy Senn <senn.jeremy@gmail.com>
2016-12-13 13:03:11 -0800

    Remove hlanding page buttons if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  helper_method :current_club
-  
-  def current_club=(club)
-    session[:club_id] = club.id
-  end
-  
-  # If i don't find a club from session i return null object
-  def current_club
-    Club.find_by(ClubCourseID: session[:club_id]) || NullClub.new
-  end
-  
+#  helper_method :current_club
+#  
+#  def current_club=(club)
+#    session[:club_id] = club.id
+#  end
+#  
+#  # If i don't find a club from session i return null object
+#  def current_club
+#    Club.find_by(ClubCourseID: session[:club_id]) || NullClub.new
+#  end
+#  
 end
