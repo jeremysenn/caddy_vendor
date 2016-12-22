64f11383900eacc29c7e4f79a162b7cae1a158ee
Jeremy Senn <senn.jeremy@gmail.com>
2016-12-22 11:31:23 -0800

    Include date and time at top of round modal

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -30,9 +30,10 @@ initialize_calendar = function() {
       select: function(start, end) {
         $.getScript('/events/new', function() {
           $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
-          date_range_picker();
+          //date_range_picker();
           $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
           $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+          $('#new_event_start').text(moment(start).format("MM/DD/YY h:mm A"));
         });
 
         calendar.fullCalendar('unselect');
@@ -56,7 +57,7 @@ initialize_calendar = function() {
       eventClick: function(event, jsEvent, view) {
         $.getScript(event.edit_url, function() {
           $('#event_date_range').val(moment(event.start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(event.end).format("MM/DD/YYYY HH:mm"))
-          date_range_picker();
+          //date_range_picker();
           $('.start_hidden').val(moment(event.start).format('YYYY-MM-DD HH:mm'));
           $('.end_hidden').val(moment(event.end).format('YYYY-MM-DD HH:mm'));
         });
