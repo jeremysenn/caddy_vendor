d775f1dcc8833711ea08beccaee85d4531a778e5
Jeremy Senn <senn.jeremy@gmail.com>
2017-01-20 12:20:57 -0800

    Add time_zone to users and allow them to edit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   helper_method :current_club
   
@@ -19,6 +20,13 @@ class ApplicationController < ActionController::Base
   # If i don't find a club from session I return the current_user company's first club.
   def current_club
     Club.find_by(ClubCourseID: session[:club_id]) || current_user.company.clubs.first
+  end
+  
+  protected
+
+  # Permit additional parameters for Devise user
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
   end
   
 end
