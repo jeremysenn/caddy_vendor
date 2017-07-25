a8d157a4c16e575f154512cb914d193ee1b5c4f7
Jeremy Senn <senn.jeremy@gmail.com>
2017-07-25 10:58:44 -0700

    Vendor payables csv file

--- app/models/vendor_payable.rb
+++ app/models/vendor_payable.rb
@@ -14,8 +14,37 @@ class VendorPayable < ApplicationRecord
     customer.caddy? unless customer.blank?
   end
   
+  def first_name
+    customer.NameF unless customer.blank?
+  end
+  
+  def last_name
+    customer.NameL unless customer.blank?
+  end
+  
+  def balance
+    self.Balance
+  end
+  
+  def time_stamp
+    Time.now
+  end
+  
   #############################
   #     Class Methods         #
   #############################
+  
+  def self.to_csv
+    require 'csv'
+    attributes = %w{time_stamp first_name last_name balance}
+    
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |vendor_payable|
+        csv << attributes.map{ |attr| vendor_payable.send(attr) }
+      end
+    end
+  end
   
 end
