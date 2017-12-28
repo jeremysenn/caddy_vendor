eb5e1d8f172ab121cc5d09d77b1b580bf20fc601
Jeremy Senn <senn.jeremy@gmail.com>
2017-12-28 08:44:37 -0800

    Confirming users through devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,7 +56,8 @@ class ApplicationController < ActionController::Base
 
   # Permit additional parameters for Devise user
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone, :admin, :active, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone, :admin, :active])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :role])
   end
   
 end
