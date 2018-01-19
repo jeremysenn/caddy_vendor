daff6caa390717318191bd980b9cd2fde59fc666
Jeremy Senn <senn.jeremy@gmail.com>
2018-01-19 11:02:36 -1000

    Balance log search by date working

--- app/controllers/balance_logs_controller.rb
+++ app/controllers/balance_logs_controller.rb
@@ -113,6 +113,6 @@ class BalanceLogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def balance_log_params
-      params.fetch(:balance_log, {}).permit(:EventID, :EventDateTime, :StartDateTime, :EndDateTime, :StartTranID, :EndTranID, :TotalAmount, :CompanyNumber)
+      params.fetch(:balance_log, {}).permit(:start_date, :end_date, :EventID, :EventDateTime, :StartDateTime, :EndDateTime, :StartTranID, :EndTranID, :TotalAmount, :CompanyNumber)
     end
 end
