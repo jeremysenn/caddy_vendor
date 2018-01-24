186ee4ee970d2fbfa1cffd5b0441ab84d44d3b6a
Jeremy Senn <senn.jeremy@gmail.com>
2018-01-24 10:55:41 -1000

    Show balance log ach report in table

--- app/controllers/balance_logs_controller.rb
+++ app/controllers/balance_logs_controller.rb
@@ -14,12 +14,14 @@ class BalanceLogsController < ApplicationController
   # GET /balance_logs/1
   # GET /balance_logs/1.json
   def show
+    require 'csv'
 #    transfers = current_user.company.transfers.where(created_at: @balance_log.StartDateTime.to_date.in_time_zone(current_user.time_zone).beginning_of_day..@balance_log.EndDateTime.to_date.in_time_zone(current_user.time_zone).end_of_day).where.not(ez_cash_tran_id: [nil, ''])
     transfers = current_user.company.transfers.where.not(ez_cash_tran_id: [nil, '']).where(ez_cash_tran_id: @balance_log.StartTranID..@balance_log.EndTranID)
     @transfers_total_amount = 0
     transfers.each do |transfer|
       @transfers_total_amount = @transfers_total_amount + transfer.amount_billed
     end
+    @ach_report = CSV.parse(@balance_log.CSVReport)
     respond_to do |format|
       format.html {
         @all_transfers = transfers
