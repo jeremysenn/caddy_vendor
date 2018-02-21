f29004490d5c904c058f51515e719f302271c764
Jeremy Senn <senn.jeremy@gmail.com>
2018-02-21 08:27:26 -0800

    On caddy show page, show last 50 rounds

--- app/controllers/caddies_controller.rb
+++ app/controllers/caddies_controller.rb
@@ -76,7 +76,8 @@ class CaddiesController < ApplicationController
     @available_balance = @caddy.available_balance
     @account = @caddy.account
 #    session[:course_id] = @caddy.course.id
-    @events = @caddy.events.order("start DESC").last(20).uniq
+#    @events = @caddy.events.order("start DESC").last(50).uniq
+    @events = @caddy.events.last(50).uniq
   end
 
   # GET /caddies/new
