3ac48bcbc623e0bc069e16ec51814abd7683452c
Jeremy <senn.jeremy@gmail.com>
2018-05-03 10:25:55 -0700

    More email settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,10 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = "http://#{ENV["APPLICATION_HOST"]}:3000"
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = true
+  
+#  config.action_mailer.perform_deliveries = true
+#  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
